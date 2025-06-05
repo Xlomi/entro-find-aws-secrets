91cdf7431de706c300fcfc892649521117816eca
Xlomi <xlomi.segal@gmail.com>
2025-06-05 15:10:11 +0300

    master- adding values to test on

--- src/test-scan.ts
+++ src/test-scan.ts
@@ -1,6 +1,9 @@
 import { scanRepo } from './services/scanner.service';
 
-const repo = 'https://github.com/trufflesecurity/trufflehog.git';
+const repo = 'https://github.com/Xlomi/entro-find-aws-secrets.git';
+// Adding here a sample secret to see if the app finds it
+// This should be found by the "AKIA" check: AKIA1EXAMPLEKEY12
+// This should be found by the "AWS Secret Access Key" check: wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
 
 (async () => {
   console.log(`Scanning repo: ${repo}...\n`);
